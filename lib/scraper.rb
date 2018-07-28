94375cb1edbb7c9b40870628107f06965d63f534
Ethel C <cofresi.ethel@gmail.com>
2018-07-28 01:37:08 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,7 +15,13 @@ class Scraper
       end
     end
     end
-  
+    
+    def get_page
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+ 
+      binding.pry
+     end
+    
 end
 
 
